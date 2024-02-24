8b1b0967dbf0eec47a9510fef580b675fe1cb5f7
Soumya Singh <soumyasingh6923@gmail.com>
2024-02-25 00:43:53 +0530

    functions

--- 03-basics/scopes.js
+++ 03-basics/scopes.js
@@ -12,8 +12,53 @@ if(true){           //block scope
 }
 
 console.log(a);
-//console.log(b);
+console.log(b);
 //console.log(c);  //var is not used because even if c is used under the scope
 //in if condition it can be called out on console while variables declared 
 //by let and const doesn't 
 
+function one(){
+    const userName = "Sam"
+
+    function two(){
+        const website = "youtube"
+        console.log(userName);
+    }
+    //console.log(website);
+
+    two()
+}
+
+one()
+
+/*when there are nested functions, usually the child functions can access 
+variables of the parent function */
+
+if(true) {
+    const username = "soumya"
+    if(username === "soumya") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+   // console.log(website);;
+}
+
+//console.log(username);
+
+//++++++++++++++++++++ interesting ++++++++++++++++++++++
+
+console.log(addOne(5)); //this one can be accessed
+function addOne(num){
+    return num + 1
+    
+}
+
+//addOne(5)
+
+addTwo(5)    //this one cannot be accessed
+const addTwo = function(num){
+    return num + 2
+    
+}
+
+//addTwo(5)
