28715754795845e8bc550fa67580ff1f02f817cd
Soumya Singh <soumyasingh6923@gmail.com>
2024-02-29 23:59:33 +0530

    loops

---
+++ 05-iterations/loops.js
@@ -0,0 +1,33 @@
+//for of
+
+//[{}, {}, {}]
+
+const  arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet  of greetings) {
+    console.log(`Each char is ${greet}`);
+}
+
+//Maps : map is an object that holds key-value pairs and remembers the 
+//original insertion order of the keys
+
+const map = new Map()      //map holds unique values
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+//console.log(map);
+
+for (const iterator of object) {
+    
+}
+
+
+
+
