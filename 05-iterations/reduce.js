6cd10c2d95306711fd04afd7a22645bafbb9b15b
Soumya Singh <soumyasingh6923@gmail.com>
2024-03-03 23:59:38 +0530

    reduce

---
+++ 05-iterations/reduce.js
@@ -0,0 +1,34 @@
+const myNums = [1,2,3]
+/*
+const myTotal = myNums.reduce(function (acc, curVal) {
+    console.log(`acc: ${acc} and curval: ${curVal}`);
+    return acc + curVal
+}, 0)   */
+
+const myTotal = myNums.reduce( (acc, curVal) => acc + curVal, 0 )
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 1999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    }
+    
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0 )
+
+console.log(priceToPay);
